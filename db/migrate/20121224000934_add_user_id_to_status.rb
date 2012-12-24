25bc74e3cfb3d414e89102e3e50c06fcd07a1102
Troy Meren <troymeren@gmail.com>
2012-12-24 08:14:17 +0800

    Installed devise for user model

---
+++ db/migrate/20121224000934_add_user_id_to_status.rb
@@ -0,0 +1,9 @@
+class AddUserIdToStatus < ActiveRecord::Migration
+  def change
+
+  	add_column :statuses, :user_id, :integer
+  	add_index :statuses, :user_id
+  	remove_column :statuses, :name
+
+  end
+end
