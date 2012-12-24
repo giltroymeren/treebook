52f586c1524020284cc46b8d8d5ebe37908fac36
Troy Meren <troymeren@gmail.com>
2012-12-24 20:27:44 +0800

    Updated profile and added scoped status updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -18,4 +18,9 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
 		assert_response :success	
 	end
 
+	test "that a profile page works" do 
+		get "/mcruz"
+		assert_response :success
+	end
+
 end
