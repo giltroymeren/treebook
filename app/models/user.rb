7f991300613f310c29ef696936a47d96849764e2
Troy Meren <troymeren@gmail.com>
2012-12-24 13:11:45 +0800

    Styled the flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :profile_name, presence: true, uniqueness: true, 
     format: {
-      with: /a-zA-Z0-9_-/,
+      with: /a-zA-Z0-9/,
       message: 'must be formatted correctly.'
     }
 
